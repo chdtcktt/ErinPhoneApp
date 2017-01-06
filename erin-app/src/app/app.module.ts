e74b1852891c59581599c165c59596ca85726961
chad <chdtcktt@gmail.com>
2017-01-06 11:49:40 -0800

    nav

--- erin-app/src/app/app.module.ts
+++ erin-app/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    HomePage,
+    AboutPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +18,8 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    HomePage,
+    AboutPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
