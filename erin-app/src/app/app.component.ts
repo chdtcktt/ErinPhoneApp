d9bcaa0a2515fde7b4f3f455cfbd618a182ea302
chad <chdtcktt@gmail.com>
2017-01-07 19:49:23 -0800

    back button works now

--- erin-app/src/app/app.component.ts
+++ erin-app/src/app/app.component.ts
@@ -14,16 +14,9 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
 
   constructor(public platform: Platform) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'About', component: AboutPage }
-    ];
 
   }
 
@@ -36,9 +29,11 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  GoHome(){
+    this.nav.push(HomePage);
+  }
+
+  GoAbout(){
+    this.nav.push(AboutPage);
   }
 }
