8302ce7f03db0b68954bbc96eb765b50c8cb5936
chad <chdtcktt@gmail.com>
2017-02-11 17:46:55 -0800

    list page search functions

--- erin-app/src/pages/list/list.ts
+++ erin-app/src/pages/list/list.ts
@@ -13,17 +13,42 @@ import { Location } from '../../app/shared/models/location'
 export class ListPage {
 
   locations: Location[];
+  unFilteredLocations: Location[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private gobleApi: GobleApi) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ListPage');
+    this.loadLocations();
+  }
 
+  loadLocations() {
     this.gobleApi.getLocations()
       .subscribe(
       result => {
         this.locations = result;
+        this.unFilteredLocations = result;
       });
+  }
+
+
+  getItems(ev: any) {
+    this.locations = this.unFilteredLocations;
+    let query = ev.target.value.toLowerCase();
+    let filteredLocations = [];
+
+    this.locations.forEach(loc => {
+
+      if (loc.name.toLowerCase().includes(query)) {
+        filteredLocations.push({
+          id: loc.id,
+          name: loc.name,
+          description: loc.description
+        });
+      }
+
+    });
+
+    this.locations = filteredLocations;
   }
 
   storeTapped($event, location) {
